f23cf3bb7f80c1c78eb1a84f74bebc7e3b87d44e
Connor Norton <connorn314@gmail.com>
2022-12-08 15:33:49 -0800

    front end for editing user info and adding prof pic

--- frontend/src/components/UserPersonalProfilePage/index.jsx
+++ frontend/src/components/UserPersonalProfilePage/index.jsx
@@ -1,9 +1,10 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import * as likeActions from '../../store/like';
 import EventIndexItem from '../EventIndexItem';
 import './UserPersonalProfilePage.css'
 import FollowIndexItem from '../FollowIndexItem';
+import { TextField } from '@mui/material';
 
 const UserPersonalProfilePage = () => {
     const dispatch = useDispatch();
@@ -13,43 +14,108 @@ const UserPersonalProfilePage = () => {
     const events = useSelector(state => state.events)
     const follows = useSelector(state => state.follows)
 
-    const followers = (user) ? user.followers : null
-    
+    const style = {
+        height: 33,
+        padding: '18px 12px 6px',
+        backgroundColor: "white",
+        border: '.5px solid rgb(188, 188, 188)',
+        borderRadius: '2px'
+    }
+
+    // const followers = (user) ? user.followers : null
+    const [editUserInfo, showEditUserInfo] = useState(false);
+    const [username, setUsername] = useState(user.username)
+    const [photoFile, setPhotoFile] = useState(null);
 
     useEffect(() => {
         dispatch(likeActions.getUserLikes())
     }, [])
 
+    const handleSubmit = () => {
+        console.log("submit")
+    }
+
+    const handleDiscard = (e) => {
+        e.preventDefault();
+        showEditUserInfo(!editUserInfo);
+        setUsername(user.username);
+        setPhotoFile(null)
+    }
+
+    const handleFile = (e) => {
+        const file = e.currentTarget.files[0];
+        setPhotoFile(file);
+    }
+
     return (
         <div id='pp-page-container'>
             <div id='pp-content-container'>
-                <div id='profile-details-container'>
-                    <div id='profile-icon-left-container'>
-                        <span class="material-symbols-rounded" id="pp-profile-icon">account_circle</span>
-                    </div>
-                    <div id='profile-details-middle-container-pp'>
-                        <div id='pp-username-container'>
-                            <div>
-                                {user.username}
+                <div id='edit-user-and-profile-details-container'>
+                    {editUserInfo ? (
+                        <form onSubmit={handleSubmit}>
+                            <div id='edit-profile-details-container'>
+                                <div id='edit-profile-general-container'>
+                                    <div id='edit-profile-icon-left-container'>
+                                        <div id='inner-edit-profile-icon-container'>
+                                            <input type="file" onChange={handleFile} />
+                                        </div>
+                                        {/* <span class="material-symbols-rounded" id="pp-profile-icon">account_circle</span> */}
+                                    </div>
+                                    <div id='pp-edit-username-container'>
+                                    <TextField
+                                        label="New username"
+                                        variant="filled"
+                                        className="user-input-box"
+                                        inputProps={{style}}
+                                        InputProps={{
+                                            disableUnderline: true
+                                        }}
+                                        InputLabelProps={{ shrink: true }}
+                                        value={username}
+                                        onChange={(e) => setUsername(e.target.value)}
+                                        />
+                                    </div>
+                                    <div id='submit-new-user-info'>
+                                        <button className="form-submit-button" id='form-submit-profile'>
+                                            Save
+                                        </button>
+                                        <div className="discard-button-profile" onClick={handleDiscard}>
+                                            Discard
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div id='profile-edit-icon-right-container'>
-                                <span class="material-symbols-rounded" id='profile-edit-icon-pp'>edit</span>
+                        </form>
+                    ) : (
+                        <div id='profile-details-container'>
+                            <div id='profile-icon-left-container'>
+                                <span class="material-symbols-rounded" id="pp-profile-icon">account_circle</span>
+                            </div>
+                            <div id='profile-details-middle-container-pp'>
+                                <div id='pp-username-container'>
+                                    <div>
+                                        {user.username}
+                                    </div>
+                                    <div id='profile-edit-icon-right-container' onClick={() => showEditUserInfo(!editUserInfo)}>
+                                        <span class="material-symbols-rounded" id='profile-edit-icon-pp'>edit</span>
+                                    </div>
+                                </div>
+                                <div id='pp-orders-likes-followers-container'>
+                                    <div id='pp-thumbnail-orders' className='pp-thumbnail-info'>
+                                        0 orders
+                                    </div>
+                                    <div className='dot-spacer' />
+                                    <div className='pp-thumbnail-info'>
+                                        {Object.values(likes).length} likes
+                                    </div>
+                                    <div className='dot-spacer' />
+                                    <div className='pp-thumbnail-info'>
+                                        {Object.values(follows).length} following
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div id='pp-orders-likes-followers-container'>
-                            <div id='pp-thumbnail-orders' className='pp-thumbnail-info'>
-                                0 Orders
-                            </div>
-                            <div className='dot-spacer' />
-                            <div className='pp-thumbnail-info'>
-                                {Object.values(likes).length} Likes
-                            </div>
-                            <div className='dot-spacer' />
-                            <div className='pp-thumbnail-info'>
-                                {followers} followers
-                            </div>
-                        </div>
-                    </div>
+                    )}
                 </div>
                 <div id='personal-activity-container'>
                     <div id='activities-cont'>
